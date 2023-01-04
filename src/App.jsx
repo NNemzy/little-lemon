698e7560e9216b53dd4f3ab88a446ec9bf75e6f0
Marian Tudose <tdsmarian98@gmail.com>
2023-01-04 15:42:06 +0100

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Specials from "./components/Specials";
 import Testimonials from "./components/Testimonials";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Specials />
       <Testimonials />
       <About />
+      <Footer />
     </>
   );
 }
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,18 +1,58 @@
 import React from "react";
+import styles from "./footer-styles.module.css";
+import img from "./../../assets/images/d1.jpeg";
 
 function Footer() {
+  const { footer, container, imageContainer, navigation, contacts, socials } =
+    styles;
+  const navigationLinks = [
+    "Home",
+    "About",
+    "Menu",
+    "Reservations",
+    "Order Online",
+    "Log In",
+  ];
+  const contactLinks = ["Address", "Phone Number", "Email"];
+  const socialLinks = ["Address", "Phone Number", "Email"];
+
   return (
-    <footer>
-      <img />
-      <ul>
-        <li>Home</li>
-      </ul>
-      <ul>
-        <li>About</li>
-      </ul>
-      <ul>
-        <li>Table</li>
-      </ul>
+    <footer className={footer}>
+      <div className={container}>
+        <div className={imageContainer}>
+          <img src={img} />
+        </div>
+        <ul className={navigation}>
+          <li>
+            <h3>Navigation Links</h3>
+          </li>
+          {navigationLinks.map((item) => (
+            <li key={item.toLowerCase()}>
+              <a href="#">{item}</a>
+            </li>
+          ))}
+        </ul>
+        <ul className={contacts}>
+          <li>
+            <h3>Contact Links</h3>
+          </li>
+          {contactLinks.map((item) => (
+            <li key={item.toLowerCase()}>
+              <a href="#">{item}</a>
+            </li>
+          ))}
+        </ul>
+        <ul className={socials}>
+          <li>
+            <h3>Social Links</h3>
+          </li>
+          {socialLinks.map((item) => (
+            <li key={item.toLowerCase()}>
+              <a href="#">{item}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </footer>
   );
 }
