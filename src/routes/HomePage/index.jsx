d5b7523a380f1be496afda1309ecccc7af2f15ed
Marian Tudose <75075189+NNemzy@users.noreply.github.com>
2023-01-12 18:25:48 +0100

    Update index.jsx

--- src/routes/HomePage/index.jsx
+++ src/routes/HomePage/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 // Components
 import Header from "../../components/Header/index.jsx";
 import Specials from "../../components/specials/index.jsx";
-import Testimonials from "../../components/testimonials.jsx";
-import About from "../../components/about.jsx";
-import Footer from "../../components/footer.jsx";
+import Testimonials from "../../components/testimonials/index.jsx";
+import About from "../../components/About/index.jsx";
+import Footer from "../../components/footer/index.jsx";
 
 function HomePage(props) {
   return (
